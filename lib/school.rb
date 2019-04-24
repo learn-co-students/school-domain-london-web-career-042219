44c67c0324776d8cee3fd125622baf6c7967ac55
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-24 20:05:14 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_writer :name
+  attr_accessor :roster
+
+
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+
+def add_student(name, year)
+
+roster[year] ||= []
+roster[year] << name
+end
+
+def grade(year)
+   roster.detect do |group, name|
+     if group == year
+       return name
+     end
+   end
+end
+
+def sort
+  new_hash = {}
+  roster.each do |group, name|
+    new_hash[group] = name.sort
+end
+new_hash
+end
+
+
+
+end
