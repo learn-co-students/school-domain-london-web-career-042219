b789d098e70ce232cd68ae36acf5acd615b4cc08
Katherine Wren <kwren@outlook.com>
2019-05-02 15:04:39 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+require 'pry'
+class School
+  
+attr_reader :name, :roster, :i
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def has_grade?(student_grade)
+    roster.include?(student_grade)
+  end
+  
+  def new_grade(student_grade)
+      roster[student_grade] = []
+  end 
+  
+  def add_student(student_name, student_grade)
+    if has_grade?(student_grade) == false
+      new_grade(student_grade)
+    end
+    roster[student_grade] << student_name
+  end
+  
+  def grade(student_grade)
+    roster[student_grade]
+  end
+  
+  
+  def sort
+    roster.each_value(&:sort!)
+  end
+  
+end
+
+puts "Mischief Managed"
